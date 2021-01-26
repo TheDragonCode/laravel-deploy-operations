eae1af0e5a66f8506dcbd3bc7f5e88b3f0121b74
Andrey Helldar <helldar@ai-rus.com>
2021-01-26 10:56:15 +0300

    Added doc-blocks

--- src/Contracts/Actionable.php
+++ src/Contracts/Actionable.php
@@ -4,7 +4,17 @@ namespace Helldar\LaravelActions\Contracts;
 
 interface Actionable
 {
+    /**
+     * Run the actions.
+     *
+     * @return void
+     */
     public function up(): void;
 
+    /**
+     * Reverse the actions.
+     *
+     * @return void
+     */
     public function down(): void;
 }
