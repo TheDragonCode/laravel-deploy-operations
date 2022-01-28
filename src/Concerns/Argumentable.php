035d5924089c7303e6cd44394a9bcff4e1521024
Andrey Helldar <helldar@ai-rus.com>
2022-01-28 22:24:03 +0300

    Add the ability to automatically generate a file name when creating

--- src/Concerns/Argumentable.php
+++ src/Concerns/Argumentable.php
@@ -2,13 +2,27 @@
 
 namespace DragonCode\LaravelActions\Concerns;
 
+use DragonCode\LaravelActions\Facades\Git;
+
 /** @mixin \Illuminate\Console\Command */
 trait Argumentable
 {
     protected function argumentName(): string
     {
-        $value = (string) $this->argument('name');
+        if ($name = (string) $this->argument('name')) {
+            return trim($name);
+        }
 
-        return trim($value);
+        return $this->getNamePrefix() . '_' . time();
+    }
+
+    protected function getNamePrefix(): string
+    {
+        return $this->getGitBranchName() ?: 'auto';
+    }
+
+    protected function getGitBranchName(): ?string
+    {
+        return Git::currentBranch(base_path('.git'));
     }
 }
