23dc5736fc38d9f478a1b615602a5ea1385c1517
Andrey Helldar <helldar@dragon-code.pro>
2022-08-17 23:11:16 +0300

    Refactoring Fresh command

--- src/Console/Fresh.php
+++ src/Console/Fresh.php
@@ -6,55 +6,35 @@ namespace DragonCode\LaravelActions\Console;
 
 use DragonCode\LaravelActions\Concerns\Optionable;
 use DragonCode\LaravelActions\Constants\Names;
-use Illuminate\Database\Console\Migrations\FreshCommand;
+use DragonCode\LaravelActions\Constants\Options;
 use Illuminate\Support\Facades\Schema;
 
-class Fresh extends FreshCommand
+class Fresh extends Command
 {
     use Optionable;
 
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
     protected $name = Names::FRESH;
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Drop and re-run all actions';
 
-    public function handle()
+    public function process(): void
     {
-        if (! $this->confirmToProceed()) {
-            return 1;
-        }
-
-        $database = $this->optionDatabase();
-
-        $this->dropTable();
-        $this->migrate($database);
-
-        return 0;
+        $this->drop();
+        $this->migrate();
     }
 
-    protected function dropTable(): void
+    protected function drop(): void
     {
         Schema::dropIfExists($this->getTableName());
     }
 
-    protected function migrate(?string $database): void
+    protected function migrate(): void
     {
-        $this->call(
-            Names::MIGRATE,
+        $this->call(Names::MIGRATE,
             array_filter([
-                '--database' => $database,
-                '--force'    => true,
-            ])
-        );
+                '--' . Options::DATABASE => $this->optionDatabase(),
+                '--' . Options::FORCE    => true,
+            ]));
     }
 
     protected function getTableName(): string
