2eb9741ada7d50f02b7901684842ad03ceeaa450
Andrey Helldar <helldar@ai-rus.com>
2022-08-18 18:19:59 +0300

    Refactored GitTest

--- tests/Services/GitTest.php
+++ tests/Services/GitTest.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace Tests\Services;
 
-use DragonCode\LaravelActions\Facades\Git;
+use DragonCode\LaravelActions\Support\Git;
 use Tests\TestCase;
 
 class GitTest extends TestCase
 {
     public function testCurrentBranchNull()
     {
-        $this->assertNull(Git::currentBranch(__DIR__));
+        $this->assertNull($this->git()->currentBranch(__DIR__));
     }
 
     public function testCurrentBranch()
     {
-        $branch = Git::currentBranch(__DIR__ . '/../../.git');
+        $branch = $this->git()->currentBranch(__DIR__ . '/../../.git');
 
         $this->assertIsString($branch);
+    }
+
+    protected function git(): Git
+    {
+        return new Git();
     }
 }
