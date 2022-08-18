a99837c505c7d4b8de961ee1d367f490499bb53a
Andrey Helldar <helldar@dragon-code.pro>
2022-08-18 20:01:42 +0300

    Update About command

--- src/Concerns/About.php
+++ src/Concerns/About.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace DragonCode\LaravelActions\Concerns;
 
 use Composer\InstalledVersions;
+use DragonCode\Support\Facades\Helpers\Arr;
+use DragonCode\Support\Facades\Helpers\Str;
 use Illuminate\Foundation\Console\AboutCommand;
 
 trait About
@@ -12,14 +14,29 @@ trait About
     protected function registerAbout(): void
     {
         if (class_exists(AboutCommand::class)) {
-            AboutCommand::add('Migration Actions', fn () => [
+            AboutCommand::add($this->getPackageName(), fn () => [
                 'Version' => $this->getPackageVersion(),
             ]);
         }
     }
 
+    protected function getPackageName(): string
+    {
+        return Str::of($this->getPackageValue('name'))
+            ->after('/')
+            ->snake()
+            ->replace('_', ' ')
+            ->title()
+            ->toString();
+    }
+
     protected function getPackageVersion(): string
     {
-        return InstalledVersions::getPrettyVersion('dragon-code/laravel-migration-actions');
+        return $this->getPackageValue('pretty_version');
+    }
+
+    protected function getPackageValue(string $key): string
+    {
+        return Arr::get(InstalledVersions::getRootPackage(), $key);
     }
 }
