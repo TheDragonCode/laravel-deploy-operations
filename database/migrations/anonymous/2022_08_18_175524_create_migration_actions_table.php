68519b1f0ccd17cdcb2ee56a83b39955ed33bd51
Andrey Helldar <helldar@ai-rus.com>
2022-08-18 18:24:54 +0300

    Refactored migrations

--- database/migrations/anonymous/2022_08_18_175524_create_migration_actions_table.php
+++ database/migrations/anonymous/2022_08_18_175524_create_migration_actions_table.php
@@ -1,30 +1,3 @@
 <?php
 
-use DragonCode\LaravelActions\Concerns\Migrations;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration {
-    use Migrations;
-
-    public function up()
-    {
-        if (Schema::hasTable($this->table())) {
-            return;
-        }
-
-        Schema::create($this->table(), function (Blueprint $table) {
-            $table->id();
-
-            $table->string('migration');
-
-            $table->integer('batch');
-        });
-    }
-
-    public function down()
-    {
-        Schema::dropIfExists($this->table());
-    }
-};
+return new class extends BaseCreateMigrationActionsTable { };
