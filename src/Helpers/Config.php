463bf1b169b262cac55512220b0d4af54f54d168
Andrey Helldar <helldar@dragon-code.pro>
2022-09-09 22:57:32 +0300

    Added Config helper

---
+++ src/Helpers/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DragonCode\LaravelActions\Helpers;
+
+class Config
+{
+    public function connection(): ?string
+    {
+        return config('database.actions.connection');
+    }
+
+    public function table(): string
+    {
+        return config('database.actions.table');
+    }
+
+    public function path(?string $path = null): string
+    {
+        $directory = config('database.actions.path', base_path('actions'));
+
+        return rtrim($directory, '\\/') . DIRECTORY_SEPARATOR . ltrim((string) $path, '\\/');
+    }
+}
