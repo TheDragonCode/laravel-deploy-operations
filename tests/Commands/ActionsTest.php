6eff86cfee934fde530edc3fc4ac27609d8695ec
Andrey Helldar <helldar@dragon-code.pro>
2023-02-15 00:00:46 +0300

    Rename test method

--- tests/Commands/ActionsTest.php
+++ tests/Commands/ActionsTest.php
@@ -11,7 +11,7 @@ use Throwable;
 
 class ActionsTest extends TestCase
 {
-    public function testMigrationCommand()
+    public function testActionsCommand()
     {
         $this->assertDatabaseDoesntTable($this->table);
 
