4ac0a1c2da119b04067a33795217569560c9a7e7
Gracjan Kubicki <gracjan@helpfind.pl>
2023-07-20 10:41:55 +0200

    main,  fixed bug

--- src/Concerns/Isolatable.php
+++ src/Concerns/Isolatable.php
@@ -25,7 +25,7 @@ trait Isolatable
 
     protected function getIsolateOption(): bool|int
     {
-        return $this->hasIsolateOption() ? $this->option(Options::ISOLATED) : false;
+        return $this->hasIsolateOption() && $this->option(Options::ISOLATED);
     }
 
     protected function hasIsolateOption(): bool
