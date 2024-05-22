9295a2d455e1f59d6829b8af9a06c6172caabea7
GitHub Action <action@github.com>
2024-05-22 19:57:13 +0000

    🧹 Fixed code-style

--- src/Constants/Names.php
+++ src/Constants/Names.php
@@ -4,13 +4,13 @@ namespace DragonCode\LaravelDeployOperations\Constants;
 
 class Names
 {
-    public const Operations  = 'operations';
-    public const Fresh       = 'operations:fresh';
-    public const Install     = 'operations:install';
-    public const Make        = 'make:operation';
-    public const Refresh     = 'operations:refresh';
-    public const Reset       = 'operations:reset';
-    public const Rollback    = 'operations:rollback';
-    public const Status      = 'operations:status';
-    public const Upgrade     = 'operations:upgrade';
+    public const Operations = 'operations';
+    public const Fresh      = 'operations:fresh';
+    public const Install    = 'operations:install';
+    public const Make       = 'make:operation';
+    public const Refresh    = 'operations:refresh';
+    public const Reset      = 'operations:reset';
+    public const Rollback   = 'operations:rollback';
+    public const Status     = 'operations:status';
+    public const Upgrade    = 'operations:upgrade';
 }
