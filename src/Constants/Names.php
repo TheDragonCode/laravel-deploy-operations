b1f2f71bac987395fcfeefcc8763801885171366
Andrey Helldar <helldar@dragon-code.pro>
2025-04-02 14:30:23 +0300

    Removed `operations:refresh` and `operations:reset` console commands

--- src/Constants/Names.php
+++ src/Constants/Names.php
@@ -10,8 +10,6 @@ class Names
     public const Fresh      = 'operations:fresh';
     public const Install    = 'operations:install';
     public const Make       = 'make:operation';
-    public const Refresh    = 'operations:refresh';
-    public const Reset      = 'operations:reset';
     public const Rollback   = 'operations:rollback';
     public const Status     = 'operations:status';
 }
